cae3910feafdab36d41169f07d82e7b991c3843a
RehanGith <chrehan1007@gmail.com>
2025-02-14 20:27:28 +0500

    Updated nav graph and Note Class

--- app/src/main/java/com/example/noteapp/Database/Note.kt
+++ app/src/main/java/com/example/noteapp/Database/Note.kt
@@ -1,5 +1,6 @@
 package com.example.noteapp.Database
 
+import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -9,4 +10,32 @@ data class Note(
     val title: String, val description: String,
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+         parcel.readInt(),
+    ) {
+
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Note> {
+        override fun createFromParcel(parcel: Parcel): Note {
+            return Note(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Note?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
