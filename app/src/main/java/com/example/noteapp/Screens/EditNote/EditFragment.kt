ecf016cce8a5cf6f312a710400750e821a916a8c
RehanGith <chrehan1007@gmail.com>
2025-02-15 00:05:21 +0500

    Created share compact to share note

--- app/src/main/java/com/example/noteapp/Screens/EditNote/EditFragment.kt
+++ app/src/main/java/com/example/noteapp/Screens/EditNote/EditFragment.kt
@@ -1,11 +1,16 @@
 package com.example.noteapp.Screens.EditNote
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.navigation.fragment.findNavController
 import com.example.noteapp.Database.Note
 import com.example.noteapp.MainActivity
@@ -27,8 +32,33 @@ class EditFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_edit, container, false)
         binding = FragmentEditBinding.bind(view)
-
+        setHasOptionsMenu(true)
         return view
+    }
+
+    private fun shareCompact() : Intent {
+        val shareIntent = ShareCompat.IntentBuilder.from(requireActivity())
+            .setType("text/plain")
+            .setChooserTitle("Share Note")
+            .setText("Title: ${note?.title}\n\nContent: ${note?.description}")
+            .intent
+        return Intent.createChooser(shareIntent, null)
+    }
+    private fun shareNote(){
+        startActivity(shareCompact())
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share, menu)
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareNote()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
